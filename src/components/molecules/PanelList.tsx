24bf17924f522ab1ccfff9b00dae9da7e93b84e3
m19e <nexus.me7290@gmail.com>
2023-03-29 22:04:37 +0900

    feat: decorate panel-item

--- src/components/molecules/PanelList.tsx
+++ src/components/molecules/PanelList.tsx
@@ -5,42 +5,13 @@ type Panel = Omit<PanelType, "students"> & { students: Student[] }
 type Props = {
   panels: Panel[]
 }
-
-export const PanelList = ({ panels }: Props) => {
-  const items = panels.map((panel) => <PanelItem key={panel.id} {...panel} />)
-
-  return <div className="grid grid-cols-3 gap-4">{items}</div>
-}
-
-const PanelItem = ({ id, title, students, href, deleted }: Panel) => {
-  return (
-    <div className="flex flex-col gap-1 p-2 rounded border">
-      <p>{id.split("-")[1]}話</p>
-      <p>{title}</p>
-      <p className="px-2 bg-pink-200 rounded">
-        {students.map((s) => s.ja).join(", ")}
-      </p>
-      <div className="flex flex-1 justify-end items-end">
-        <a
-          className="py-1 px-2 font-bold text-white bg-blue-500 rounded link"
-          href={href}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Tweet Link
-        </a>
-      </div>
-    </div>
-  )
-}
-
 export const SkewedPanelList = ({ panels }: Props) => {
   const items = panels.map((panel) => (
     <SkewedPanelItem key={panel.id} {...panel} />
   ))
 
   return (
-    <div className="overflow-y-scroll p-2 my-2 space-y-2 w-full max-w-sm h-64 bg-gray-100 border skewed-list scrollbar-hidden">
+    <div className="overflow-y-scroll p-2 my-2 space-y-2 w-full max-w-md h-64 bg-gray-600 skewed-list scrollbar-hidden">
       {items}
     </div>
   )
@@ -48,42 +19,52 @@ export const SkewedPanelList = ({ panels }: Props) => {
 
 const SkewedPanelItem = ({ id, title, students, href, deleted }: Panel) => {
   return (
-    <div className="flex overflow-hidden relative gap-1 items-center px-6 h-14 bg-white border-l-4 border-sky-300 skewed-item">
-      <div className="absolute bottom-0 -left-8 z-0 skew-x-[30deg]">
-        <div className="triangle"></div>
-      </div>
-      <div className="absolute bottom-0 left-[2px] z-10">
-        <p className="text-2xs">00{id.split("-")[1]}</p>
-      </div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-6 h-6 skew-x-[30deg]"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0120.25 6v12A2.25 2.25 0 0118 20.25H6A2.25 2.25 0 013.75 18V6A2.25 2.25 0 016 3.75h1.5m9 0h-9"
-        />
-      </svg>
-
-      <p className="text-sm">{title}</p>
-      {/* <div className="px-2 bg-pink-200 rounded">
-        <p>{students.map((s) => s.ja).join(", ")}</p>
-      </div> */}
+    <div className="flex overflow-hidden relative gap-1 items-center pt-2 pr-8 pb-3 pl-6 font-rounded bg-white rounded-sm border-l-4 border-sky-300 skewed-item">
+      <Episode id={id} />
+      <p className="font-medium">{title}</p>
       <div className="flex flex-1 justify-end">
         <a
-          className="flex justify-center py-1.5 w-16 bg-sky-300 rounded-sm"
+          className="flex justify-center py-2 w-24 bg-sky-300 rounded-sm"
           href={href}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <p className="text-sm font-medium">入場</p>
+          <p className="font-medium">入場</p>
         </a>
       </div>
+      <div className="absolute top-0 -right-8 z-0 skew-x-[30deg]">
+        <div className="bg-gray-100 rotate-180 triangle"></div>
+      </div>
     </div>
+  )
+}
+
+const getEpisodeFromID = (id: string) => {
+  const digit3 = id.split("-")[1]
+  const is3 = digit3.length === 3
+  const digit2 = ("00" + digit3).slice(-2)
+
+  return { digit3, digit2, is3 }
+}
+
+const Episode = ({ id }: { id: string }) => {
+  const { is3, digit2, digit3 } = getEpisodeFromID(id)
+  const triangleLeft = is3 ? "-left-7" : "-left-8"
+
+  return (
+    <>
+      <div className={`absolute bottom-0 z-0 skew-x-[30deg] ${triangleLeft}`}>
+        <div className="bg-sky-100 triangle"></div>
+      </div>
+      {is3 ? (
+        <div className="absolute bottom-0 left-[2px] z-10">
+          <p className="font-medium text-center text-2xs">{digit3}</p>
+        </div>
+      ) : (
+        <div className="absolute bottom-0 left-1 z-10">
+          <p className="text-xs font-medium text-center">{digit2}</p>
+        </div>
+      )}
+    </>
   )
 }
