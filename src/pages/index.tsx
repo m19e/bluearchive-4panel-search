c112891be2bd960ebbdb3c5566c01a8d20082e2f
m19e <nexus.me7290@gmail.com>
2023-03-29 21:11:33 +0900

    feat: remove panel-list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,33 @@
 import type { NextPage, InferGetServerSidePropsType } from "next"
 import { getAllPanels } from "@/utils"
 
-import { PanelList, SkewedPanelList } from "@/components/molecules/PanelList"
+import { SkewedPanelList } from "@/components/molecules/PanelList"
 
 type Props = InferGetServerSidePropsType<typeof getServerSideProps>
 
 const Page: NextPage<Props> = ({ panels }) => {
   return (
     <>
-      <div className="flex justify-center w-full">
-        <SkewedPanelList panels={panels.aoharu} />
-      </div>
       <div className="p-4 space-y-4">
         <h1 className="text-3xl">ブルアカ4コマ検索</h1>
         <h2 className="text-xl">ぶるーあーかいぶっ！</h2>
-        <PanelList panels={panels.ja} />
+        <div className="flex justify-center w-full">
+          <SkewedPanelList panels={panels.ja} />
+        </div>
         <div className="my-2 w-full h-[1px] bg-black"></div>
         <h2 className="text-xl">Blue Archive Official 4-Panel Manga</h2>
-        <PanelList panels={panels.en} />
+        <div className="flex justify-center w-full">
+          <SkewedPanelList panels={panels.en} />
+        </div>
         <div className="my-2 w-full h-[1px] bg-black"></div>
         <h2 className="text-xl">あおはるレコード</h2>
-        <PanelList panels={panels.aoharu} />
+        <div className="flex justify-center w-full">
+          <SkewedPanelList panels={panels.aoharu} />
+        </div>
       </div>
     </>
   )
 }
-
 
 export const getServerSideProps = async () => {
   const panels = await getAllPanels()
