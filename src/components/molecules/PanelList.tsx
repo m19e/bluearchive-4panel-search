81361e52115ff4c44782b8464f23c7d0c096c192
m19e <nexus.me7290@gmail.com>
2023-03-30 19:43:02 +0900

    refactor: extract marks to atom

--- src/components/molecules/PanelList.tsx
+++ src/components/molecules/PanelList.tsx
@@ -1,6 +1,7 @@
 import type { Panel as PanelType, Student } from "@/types"
 
 import { Episode } from "@/components/molecules/Episode"
+import { Marks } from "@/components/atoms/Marks"
 
 type Panel = Omit<PanelType, "students"> & { students: Student[] }
 
@@ -17,61 +18,6 @@ export const SkewedPanelList = ({ title, panels }: Props) => {
     <div className="overflow-y-scroll p-2 my-2 space-y-2 w-full max-w-md h-64 min-h-screen bg-sky-100 skewed-list scrollbar-hidden">
       <Category title={title} />
       {items}
-    </div>
-  )
-}
-
-const Marks = () => {
-  return (
-    <div className="flex items-center h-6 text-gray-300">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={3}
-        stroke="currentColor"
-        className="w-3 h-3"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-        />
-      </svg>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={3}
-        stroke="currentColor"
-        className="w-4 h-4"
-      >
-        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
-      </svg>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={4}
-        stroke="currentColor"
-        className="w-3 h-3"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M6 18L18 6M6 6l12 12"
-        />
-      </svg>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        stroke="currentColor"
-        fill="currentColor"
-        strokeWidth={2}
-        viewBox="0 0 16 16"
-        className="mx-1 w-2 h-2"
-      >
-        <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z"></path>
-      </svg>
     </div>
   )
 }
