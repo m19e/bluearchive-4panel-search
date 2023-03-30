42d45fb18f911a8c9b91729abb8095fcf379a595
m19e <nexus.me7290@gmail.com>
2023-03-30 16:56:09 +0900

    feat: sort newer panels

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ const Page: NextPage<Props> = ({ panels }) => {
         <h1 className="text-3xl">ブルアカ4コマ検索</h1>
         <h2 className="text-xl">ぶるーあーかいぶっ！</h2>
         <div className="flex justify-center w-full">
-          <SkewedPanelList panels={panels.ja} />
+          <SkewedPanelList panels={panels.ja.reverse()} />
         </div>
         <div className="my-2 w-full h-[1px] bg-black"></div>
         <h2 className="text-xl">Blue Archive Official 4-Panel Manga</h2>
