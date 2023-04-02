6d195ea8c14d15d0c87268adc784fed4e6d5711e
m19e <nexus.me7290@gmail.com>
2023-04-02 19:48:37 +0900

    chore: add school colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,24 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        kivotos: "#00AEE1",
+        prime_student_council: "#00AEE1",
+        etc: "#00aee1",
+        abydos: "#2a928f",
+        gehenna: "#a6465c",
+        millennium: "#3c93bf",
+        trinity: "#c39650",
+        hyakkiyako: "#d65b8b",
+        shanhaijing: "#4b8f60",
+        red_winter: "#8b415a",
+        valkyrie: "#a1a9e1",
+        // arius: "#dbdad8",
+        arius: "#6D6E6C",
+        srt: "#90abc9",
+        kronos: "#f5eb75",
+        others_students: "#000",
+      },
       fontFamily: {
         latego: ["GenEiLateGo"],
         koburi: ["GenEiKoburiMin"],
