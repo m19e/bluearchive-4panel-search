c40c9f382e8fc5b06c048660e6205605c239bc88
m19e <nexus.me7290@gmail.com>
2023-04-07 17:42:28 +0900

    feat: add hooks about student

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,20 @@
-export {}
+import { useAtom } from "jotai"
+
+import type { Student } from "@/types"
+import { groupedStudentsAtom, selectedStudentsAtom } from "@/stores"
+
+export const useGroupedStudents = () => useAtom(groupedStudentsAtom)
+export const useSelectedStudents = () => {
+  const [selectedStudents, setSelected] = useAtom(selectedStudentsAtom)
+
+  const select = (student: Student) => setSelected((prev) => [...prev, student])
+  const unselect = (student: Student) =>
+    setSelected((prev) => [...prev.filter((s) => s.id !== student.id)])
+
+  const update = (student: Student) => {
+    const isSelected = selectedStudents.some((s) => s.id === student.id)
+    isSelected ? unselect(student) : select(student)
+  }
+
+  return { selectedStudents, update }
+}
