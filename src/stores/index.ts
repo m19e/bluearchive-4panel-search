36217bd9d67f2ba76b8a7e030328a02f45aa2306
m19e <nexus.me7290@gmail.com>
2023-04-15 22:54:54 +0900

    refactor: add all-panels atom

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,9 +1,14 @@
 import { atom } from "jotai"
 
-import type { GroupedStudents, Student } from "@/types"
+import type { GroupedStudents, PanelData, Student } from "@/types"
 
 // Atoms
 
+export const allPanelsAtom = atom<{
+  ja: PanelData[]
+  en: PanelData[]
+  aoharu: PanelData[]
+}>({ ja: [], en: [], aoharu: [] })
 export const groupedStudentsAtom = atom<GroupedStudents[]>([])
 export const selectedStudentsAtom = atom<Student[]>([])
 
