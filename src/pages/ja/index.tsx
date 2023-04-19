2f05d16250a5e8c5ffd3c321a5bcabe249f5e5fd
m19e <nexus.me7290@gmail.com>
2023-04-19 19:09:25 +0900

    feat: add /ja, /en pages

---
+++ src/pages/ja/index.tsx
@@ -0,0 +1,31 @@
+import type { InferGetServerSidePropsType, NextPage } from "next"
+
+import { getAllData, HydrateAtoms } from "@/utils"
+import { allPanelsAtom, langAtom } from "@/stores"
+
+import { TopPage } from "@/components/templates/TopPage"
+
+type Props = InferGetServerSidePropsType<typeof getServerSideProps>
+
+const Page: NextPage<Props> = ({ panels, students }) => {
+  return (
+    <HydrateAtoms
+      initialValues={[
+        [allPanelsAtom, panels],
+        [langAtom, "ja"],
+      ]}
+    >
+      <TopPage students={students} />
+    </HydrateAtoms>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const props = await getAllData()
+
+  return {
+    props,
+  }
+}
+
+export default Page
