657ac23f281f157837f217a19d7f3a7b2fbb113f
m19e <nexus.me7290@gmail.com>
2023-04-20 17:09:11 +0900

    feat: locale is 'en' or 'ja'

--- src/hooks/useLocale.ts
+++ src/hooks/useLocale.ts
@@ -3,7 +3,8 @@ import en from "@/consts/locales/en"
 import ja from "@/consts/locales/ja"
 
 export const useLocale = () => {
-  const { locale } = useRouter()
-  const t = locale === "en" ? en : ja
+  const { locale: lcl } = useRouter()
+  const t = lcl === "en" ? en : ja
+  const locale = lcl === "en" ? "en" : "ja"
   return { locale, t }
 }
