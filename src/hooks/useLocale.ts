4b2a802abfc1c868ca0c1740659bffabfadfb328
m19e <nexus.me7290@gmail.com>
2023-04-20 17:44:53 +0900

    feat: declare is-en

--- src/hooks/useLocale.ts
+++ src/hooks/useLocale.ts
@@ -1,10 +1,13 @@
 import { useRouter } from "next/router"
+import type { Lang } from "@/types"
 import en from "@/consts/locales/en"
 import ja from "@/consts/locales/ja"
 
 export const useLocale = () => {
   const { locale: lcl } = useRouter()
-  const t = lcl === "en" ? en : ja
-  const locale = lcl === "en" ? "en" : "ja"
+  const isEn = lcl === "en"
+
+  const t = isEn ? en : ja
+  const locale: Lang = isEn ? "en" : "ja"
   return { locale, t }
 }
