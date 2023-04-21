3efea0c664acd08f6e13509be86574a2230650a4
m19e <nexus.me7290@gmail.com>
2023-04-21 20:18:18 +0900

    feat: add snippet for load twitter-wjs

--- src/components/templates/TopPage.tsx
+++ src/components/templates/TopPage.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script"
+
 import type { GroupedStudents } from "@/types"
 
 import { ModalDialog } from "@/components/organisms/Modal/Dialog"
@@ -12,6 +14,26 @@ type Props = {
 export const TopPage = ({ students }: Props) => {
   return (
     <>
+      <Script id="load-twitter-wjs" strategy="lazyOnload">
+        {`
+        window.twttr = (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0],
+            t = window.twttr || {};
+          if (d.getElementById(id)) return t;
+          js = d.createElement(s);
+          js.id = id;
+          js.src = "https://platform.twitter.com/widgets.js";
+          fjs.parentNode.insertBefore(js, fjs);
+
+          t._e = [];
+          t.ready = function(f) {
+            t._e.push(f);
+          };
+
+          return t;
+        }(document, "script", "twitter-wjs"));
+        `}
+      </Script>
       <div className="flex flex-col min-h-screen font-rounded bg-triangle">
         <div className="flex-1">
           <PanelContainer />
