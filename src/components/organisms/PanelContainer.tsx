c6170619df6405aec7ff2c93aaa07626923f1c39
m19e <nexus.me7290@gmail.com>
2023-04-22 02:30:14 +0900

    feat: locale about share

--- src/components/organisms/PanelContainer.tsx
+++ src/components/organisms/PanelContainer.tsx
@@ -30,9 +30,11 @@ export const PanelContainer = () => {
                 </p>
               </a>
             </Link>
-            <div className="flex gap-3 items-center">
-              {/* <a
-                className="flex gap-3 items-center py-1 px-1.5 -my-1 -mx-1.5 text-[0.8125rem] font-extrabold leading-6 text-slate-900 rounded-lg"
+            <div className="flex gap-2 items-center">
+              <LangNav />
+              <span className="text-slate-600/50">/</span>
+              <a
+                className="flex gap-2 items-center py-1 px-1.5 -my-1 -mx-1.5 text-[0.8125rem] font-extrabold leading-6 text-slate-900 rounded-lg"
                 href=""
               >
                 <svg
@@ -42,13 +44,8 @@ export const PanelContainer = () => {
                 >
                   <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0 0 20 3.92a8.19 8.19 0 0 1-2.357.646 4.118 4.118 0 0 0 1.804-2.27 8.224 8.224 0 0 1-2.605.996 4.107 4.107 0 0 0-6.993 3.743 11.65 11.65 0 0 1-8.457-4.287 4.106 4.106 0 0 0 1.27 5.477A4.073 4.073 0 0 1 .8 7.713v.052a4.105 4.105 0 0 0 3.292 4.022 4.095 4.095 0 0 1-1.853.07 4.108 4.108 0 0 0 3.834 2.85A8.233 8.233 0 0 1 0 16.407a11.615 11.615 0 0 0 6.29 1.84"></path>
                 </svg>
-
-                <span className="sr-only sm:not-sr-only">Share on Twitter</span>
+                <span className="sr-only sm:not-sr-only">{t.SHARE}</span>
               </a>
-              <span className="text-slate-600/50">/</span> */}
-              <div className="flex gap-2 items-center">
-                <LangNav />
-              </div>
             </div>
           </header>
         </div>
