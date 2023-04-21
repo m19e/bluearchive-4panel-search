f62c3f473482f2f267558770decf3713c0e7996e
m19e <nexus.me7290@gmail.com>
2023-04-21 21:51:41 +0900

    feat: add ogp

--- src/components/templates/TopPage.tsx
+++ src/components/templates/TopPage.tsx
@@ -1,7 +1,9 @@
+import Head from "next/head"
 import Script from "next/script"
 
 import type { GroupedStudents } from "@/types"
 
+import { useLocale } from "@/hooks/useLocale"
 import { ModalDialog } from "@/components/organisms/Modal/Dialog"
 import { PanelContainer } from "@/components/organisms/PanelContainer"
 import { Search } from "@/components/organisms/Search"
@@ -12,6 +14,8 @@ type Props = {
 }
 
 export const TopPage = ({ students }: Props) => {
+  const { t } = useLocale()
+
   return (
     <>
       <Script id="load-twitter-wjs" strategy="lazyOnload">
@@ -34,6 +38,14 @@ export const TopPage = ({ students }: Props) => {
         }(document, "script", "twitter-wjs"));
         `}
       </Script>
+      <Header
+        title={t.META_TITLE}
+        description={t.META_DESCRIPTION}
+        ogImage={process.env.NEXT_PUBLIC_SITE_ROOT_URL + "hoshino.png"}
+        ogTitle={t.OG_TITLE}
+        twUrl={process.env.NEXT_PUBLIC_SITE_ROOT_URL ?? ""}
+        twCard="summary"
+      />
       <div className="flex flex-col min-h-screen font-rounded bg-triangle">
         <div className="flex-1">
           <PanelContainer />
@@ -45,3 +57,36 @@ export const TopPage = ({ students }: Props) => {
     </>
   )
 }
+
+type HeaderProps = {
+  title: string
+  description: string
+  ogImage: string
+  ogTitle: string
+  twUrl: string
+  twCard: "summary"
+}
+
+const Header = ({
+  title,
+  description,
+  ogTitle,
+  ogImage,
+  twUrl,
+  twCard,
+}: HeaderProps) => (
+  <Head>
+    <title>{title}</title>
+    <link rel="icon" href="/favicon.ico" />
+    <meta name="description" content={description} />
+    <meta property="og:title" content={ogTitle} />
+    <meta property="og:description" content={description} />
+    <meta property="og:image" content={ogImage} />
+    <meta property="og:type" content="article" />
+    <meta property="twitter:title" content={ogTitle} />
+    <meta property="twitter:description" content={description} />
+    <meta property="twitter:image" content={ogImage} />
+    <meta property="twitter:url" content={twUrl} />
+    <meta property="twitter:card" content={twCard} />
+  </Head>
+)
--- src/consts/locales/ja.ts
+++ src/consts/locales/ja.ts
@@ -8,5 +8,9 @@ const ja = {
   STUDENTS: "登場生徒",
   OFFICIAL: "公式サイト",
   SEARCH_RESULT: "検索結果",
+  META_TITLE: "ブルアカ4コマ検索 | Blue Archive 4-Panel Search",
+  META_DESCRIPTION:
+    "ブルーアーカイブの公式4コマ漫画(日本版、グローバル版、あおはるレコード)を検索できるサービスです。",
+  OG_TITLE: "ブルアカ4コマ検索",
 }
 export default ja
