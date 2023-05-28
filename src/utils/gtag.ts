d9cc0196e143025d64a898ed246f08c898d0f377
m19e <nexus.me7290@gmail.com>
2023-05-28 19:11:23 +0900

    feat: gtag util

---
+++ src/utils/gtag.ts
@@ -0,0 +1,27 @@
+export const GA_ID = process.env.NEXT_PUBLIC_GA_ID || ""
+
+export const pageview = (url: string) => {
+  if (!GA_ID) return
+  window.gtag("config", GA_ID, {
+    page_path: url,
+  })
+}
+
+export const event = ({
+  action,
+  category,
+  label,
+  value,
+}: {
+  action: string
+  category: string
+  label: string
+  value?: number
+}): void => {
+  if (!GA_ID) return
+  window.gtag("event", action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  })
+}
